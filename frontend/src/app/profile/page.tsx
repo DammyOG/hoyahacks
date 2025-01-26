810fff93c0986e9063eed86195da1eebfcc14e18
Fadeela Ali <fadeelaali43@gmail.com>
2025-01-25 22:42:40 -0500

    added a defaultprofile page

---
+++ frontend/src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props {
+  // Define your props here, e.g.:
+  title?: string; // Optional prop
+}
+
+export default function Profile() {
+  return (
+    <div>
+      <h1>{'hey'}</h1>
+    </div>
+  )
+}
