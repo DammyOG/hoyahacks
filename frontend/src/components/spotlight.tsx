d9b66fc42191e1e8d063fc5da0201a78b97eae61
Fadeela Ali <fadeelaali43@gmail.com>
2025-01-26 03:36:19 -0500

    home page card redesign

--- frontend/src/components/spotlight.tsx
+++ frontend/src/components/spotlight.tsx
@@ -6,27 +6,27 @@ const Spotlight = () => {
     return (
         <div className='w-full xl:w-3/5 flex flex-col gap-4 ml-4 text-white'>
             <div className='space-y-4'>
-                <CardTitle> Spotlight</CardTitle>
-                <div className='flex gap-3 oveflow-x-auto'>
-                    <HomeCard title='King' content='Hola' img='/images/icon.png' />
-                    <HomeCard title='King' content='Hola' img='/images/icon.png' />
-                    <HomeCard title='King' content='Hola' img='/images/image.png' />
+                <CardTitle className='pt-10'> Spotlight</CardTitle>
+                <div className='flex pl-3 gap-9 oveflow-x-auto'>
+                    <HomeCard title='Sample Project Name' content='This project does things. Things are important. Things inspire.' img='/images/tech.jpeg' />
+                    <HomeCard title='Sample Project Name' content='This project does things. Things are important. Things inspire.' img='/images/tech.jpeg' />
+                    <HomeCard title='Sample Project Name' content='This project does things. Things are important. Things inspire.' img='/images/tech.jpeg' />
                 </div>
             </div>
             <div className='space-y-4'>
-                <CardTitle>Mobile App Development</CardTitle>
-                <div className='flex gap-3'>
-                    <HomeCard title='King' content='Hola' img='/images/icon.png' />
-                    <HomeCard title='King' content='Hola' img='/images/icon.png' />
-                    <HomeCard title='King' content='Hola' img='/images/image.png' />
+                <CardTitle className='pt-10'>Mobile App Development</CardTitle>
+                <div className='flex pl-3 gap-9 oveflow-x-auto'>
+                    <HomeCard title='Sample Project Name' content='This project does things. Things are important. Things inspire.' img='/images/tech.jpeg' />
+                    <HomeCard title='Sample Project Name' content='This project does things. Things are important. Things inspire.' img='/images/tech.jpeg' />
+                    <HomeCard title='Sample Project Name' content='This project does things. Things are important. Things inspire.' img='/images/tech.jpeg' />
                 </div>
             </div>
             <div className='space-y-4 '>
-                <CardTitle> Web Development</CardTitle>
-                <div className='flex gap-3'>
-                    <HomeCard title='King' content='Hola' img='/images/icon.png' />
-                    <HomeCard title='King' content='Hola' img='/images/icon.png' />
-                    <HomeCard title='King' content='Hola' img='/images/image.png' />
+                <CardTitle className='pt-10'> Web Development</CardTitle>
+                <div className='flex pl-3 gap-9 oveflow-x-auto'>
+                    <HomeCard title='Sample Project Name' content='This project does things. Things are important. Things inspire.' img='/images/tech.jpeg' />
+                    <HomeCard title='Sample Project Name' content='This project does things. Things are important. Things inspire.' img='/images/tech.jpeg' />
+                    <HomeCard title='Sample Project Name' content='Hola' img='/images/tech.jpeg' />
                 </div>
             </div>
         </div >
